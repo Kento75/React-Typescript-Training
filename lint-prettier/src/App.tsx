57171b4da5d7c4d60299a9557d66f01108b71b87
Kento75 <kento2github@gmail.com>
2019-04-14 19:09:39 +0900

    React and eslint disable

--- lint-prettier/src/App.tsx
+++ lint-prettier/src/App.tsx
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello, World!!!
+            Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
             className="App-link"
