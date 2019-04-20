a2b80c520cb1bb3c67abddc7998b2491563ed34a
Kento75 <kento2github@gmail.com>
2019-04-20 14:31:44 +0900

    Add custom hooks

---
+++ 08-Customhooks/src/containers/App.tsx
@@ -0,0 +1,31 @@
+import React, {FC, useEffect, useState} from "react";
+
+import AppComponent from "../components/App";
+
+const useTimer = (limitSec: number): [number, () => void] => {
+  const [timeLeft, setTimeLeft] = useState(limitSec);
+
+  const reset = () => {
+    setTimeLeft(limitSec);
+  }
+
+  const tick = () => {
+    setTimeLeft(prevTime => (prevTime === 0 ? limitSec : prevTime - 1));
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(tick, 1000);
+
+    return () => clearInterval(timerId);
+  }, []);
+  return [timeLeft, reset];
+};
+
+const AppContainer: FC = () => {
+  const LIMIT = 60;
+  const [timeLeft, reset] = useTimer(LIMIT);
+
+  return <AppComponent timeLeft={timeLeft} reset={reset} />
+}
+
+export default AppContainer;
