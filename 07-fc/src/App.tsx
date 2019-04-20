0922838030ebb90352e50d31ffb414c296c59871
Kento75 <kento2github@gmail.com>
2019-04-20 12:49:28 +0900

    Add 07-fc

---
+++ 07-fc/src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import CharacterList, { Character } from './CharacterList';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    const characters: Character[] = [
+      {
+        id: 1,
+        name: '新垣結衣',
+        age: 30,
+        height: 170,
+      },
+      {
+        id: 2,
+        name: '本田翼',
+        age: 26,
+        height: 165,
+      },
+      {
+        id: 3,
+        name: 'Kento75',
+        age: 25,
+      },
+    ]
+    
+    return (
+      <div className="container">
+        <header>
+          <h1>メンバー一覧</h1>
+        </header>
+        <CharacterList school="Node学園" characters={characters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
